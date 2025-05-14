33c4a33e4149f7a57c50359a19d467798ec48e02
CaIIister <tarashhh@gmail.com>
2025-05-14 16:05:51 +0300

    4

--- two_stage_detection.py
+++ two_stage_detection.py
@@ -129,7 +129,7 @@ class CustomTransforms:
         image = T.ToTensor()(image)
 
         # Normalize
-        image = transforms.Normalize(
+        image = T.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]
         )(image)
